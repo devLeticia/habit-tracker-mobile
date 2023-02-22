47e70d8a8635e8eabddf39872cd34765a236dafd
Leticia Gonçalves <leticiagoncalves.tech@gmail.com>
2023-02-22 19:44:25 -0300

    habits tracker layout, tailwind config, icons and svg support

--- App.tsx
+++ App.tsx
@@ -7,8 +7,8 @@ import {
   Inter_700Bold,
   Inter_800ExtraBold,
 } from '@expo-google-fonts/inter'
-
-import { Loading } from './src/assets/components/Loading'
+import { Loading } from './src/components/Loading'
+import { Home } from './src/screens/Home'
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -23,28 +23,13 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
+    <>
       <StatusBar
         barStyle='light-content'
         backgroundColor='transparent'
         translucent
       />
-      <Text style={styles.text}>
-        Open up App.tsx to start working on your app!
-      </Text>
-    </View>
+      <Home />
+    </>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#09090a',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#fff',
-    fontFamily: 'Inter_700Bold',
-  },
-})
---
+++ src/lib/dayjs.ts
@@ -0,0 +1,4 @@
+import dayjs from 'dayjs'
+import 'dayjs/locale/ptbr'
+
+dayjs.locale('pt-br')
